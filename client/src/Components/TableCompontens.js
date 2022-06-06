2e3d368ebc978e6e5aa733a85791889f38a32cfe
VINCENZO MEZZELA S294999 <s294999@studenti.polito.it>
2022-06-06 21:28:45 +0200

    added expandable rows

--- client/src/Components/TableCompontens.js
+++ client/src/Components/TableCompontens.js
@@ -1,14 +1,77 @@
-import { Table } from "react-bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { useState } from "react";
+import { Table, Button, Tooltip, OverlayTrigger } from "react-bootstrap";
 
 function CourseRow(props) {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <tr>
-      <td>{props.code}</td>
-      <td>{props.name}</td>
-      <td>{props.credits}</td>
-      <td>{props.currentStudentsNumber}</td>
-      <td>{props.maxStudentsNumber}</td>
-    </tr>
+    <>
+      <tr>
+        <td>{props.code}</td>
+        <td>{props.name}</td>
+        <td>{props.credits}</td>
+        <td>{props.currentStudentsNumber}</td>
+        <td>{props.maxStudentsNumber}</td>
+        {props.loggedIn && (
+          <td>
+            <IncompatibleExam />
+            <Button size="sm" variant="outline-success" className="ms-1">
+              <i className="bi bi-plus"></i>
+            </Button>
+            <Button
+              size="sm"
+              variant="outline-primary"
+              className="ms-1"
+              onClick={() => setExpanded((e) => !e)}
+            >
+              <i
+                className={expanded ? "bi bi-caret-up" : "bi bi-caret-down"}
+              ></i>
+            </Button>
+          </td>
+        )}
+      </tr>
+      {expanded && (
+        //try with table inside table
+        <tr>
+          <td colSpan={6}>
+            <ul className="ms-4 ">
+              {props.incompatibleCourses.length > 0 && (
+                <li>
+                  incompatible courses :{" "}
+                  {props.incompatibleCourses.map(
+                    (course, index) =>
+                      course +
+                      (index == props.incompatibleCourses.length - 1
+                        ? ""
+                        : ", ")
+                  )}
+                </li>
+              )}
+              {props.preparatoryCourse && <li>{props.preparatoryCourse}</li>}
+            </ul>
+          </td>
+        </tr>
+      )}
+    </>
+  );
+}
+
+function IncompatibleExam(props) {
+  return (
+    <>
+      <OverlayTrigger placement="right" overlay={<Tooltip>Tooltip</Tooltip>}>
+        {!props.ok ? (
+          <Button size="sm" variant="danger">
+            <i className="bi bi-exclamation-circle"></i>
+          </Button>
+        ) : (
+          <Button size="sm" variant="outline-succes">
+            <i className="bi bi-check2"></i>
+          </Button>
+        )}
+      </OverlayTrigger>
+    </>
   );
 }
 
@@ -22,6 +85,7 @@ function CourseTable(props) {
           <th>credits</th>
           <th>current students number</th>
           <th>max students number</th>
+          {props.loggedIn && <th></th>}
         </tr>
       </thead>
 
@@ -29,6 +93,7 @@ function CourseTable(props) {
         {props.courses.map((course) => {
           return (
             <CourseRow
+              loggedIn={props.loggedIn}
               key={course.code}
               code={course.code}
               name={course.name}
