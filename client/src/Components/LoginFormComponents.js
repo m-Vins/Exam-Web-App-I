ceebf9d236e473162f1b47a5dcc16e8d2b4ec4a7
VINCENZO MEZZELA S294999 <s294999@studenti.polito.it>
2022-06-06 19:57:54 +0200

    added login form component

---
+++ client/src/Components/LoginFormComponents.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Form, Row, Button, Col, Container } from "react-bootstrap";
+import "../App.css";
+
+function LogInForm(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    return "";
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="username" className="mb-4">
+        <Form.Label>Email</Form.Label>
+        <Form.Control
+          type="email"
+          value={username}
+          onChange={(ev) => setUsername(ev.target.value)}
+          required={true}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="password" className="mb-5">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+          required={true}
+          minLength={6}
+        />
+      </Form.Group>
+
+      <Button
+        variant="outline-success"
+        className="mt-3 loginButton"
+        type="submit"
+      >
+        Login
+      </Button>
+    </Form>
+  );
+}
+
+export default LogInForm;
