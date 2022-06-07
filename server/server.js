d6190f3b8895578e66a97fe1aa1afff927cdf7f1
VINCENZO MEZZELA S294999 <s294999@studenti.polito.it>
2022-06-07 10:30:24 +0200

    added auth management in server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const session = require("express-session");
+
 const Services = require("./services");
 
 const services = new Services();
@@ -16,6 +20,37 @@ const corsOptions = {
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors(corsOptions));
+
+passport.use(
+  new LocalStrategy(async function verify(username, password, cb) {
+    const student = await services.getStudent(username, password);
+    if (!student) return cb(null, false);
+    return cb(null, student);
+  })
+);
+
+passport.serializeUser(function (student, cb) {
+  cb(null, student);
+});
+
+passport.deserializeUser(function (student, cb) {
+  return cb(null, student);
+});
+
+app.use(
+  session({
+    secret: `It's a secret`,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.authenticate("session"));
+
+const isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) return next();
+  return res.status(401).end();
+};
 
 // activate the server
 app.listen(port, () =>
@@ -36,9 +71,9 @@ app.get("/api/students", (req, res) => {
     .catch((err) => res.status(500).json(err));
 });
 
-app.get("/api/students/:studentID/studyplan", (req, res) => {
+app.get("/api/studyplan", isLoggedIn, (req, res) => {
   services
-    .getStudyPlan(req.params.studentID)
+    .getStudyPlan(req.user.id)
     .then((courses) => res.status(200).json(courses))
     .catch((err) => res.status(500).json(err));
 });
@@ -52,4 +87,23 @@ app.post("/api/students/:studentID/studyplan", (req, res) => {
         ? res.status(err.code).json(err)
         : res.status(500).json(err)
     );
+});
+
+app.post("/api/sessions", passport.authenticate("local"), (req, res) => {
+  console.log(req.user);
+  res.status(201).json(req.user);
+});
+
+app.delete("/api/sessions/current", (req, res) => {
+  req.logout(() => {
+    res.end();
+  });
+});
+
+app.get("/api/sessions/current", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.json(req.user);
+  } else {
+    res.status(401).json({ error: "Not authenticated" });
+  }
 });
