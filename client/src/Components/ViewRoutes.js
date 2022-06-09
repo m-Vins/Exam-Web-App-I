dd22cd5f466d2278a90a3ededff3515d008f166a
VINCENZO MEZZELA S294999 <s294999@studenti.polito.it>
2022-06-09 20:28:25 +0200

    PersonalHomeRoute update

--- client/src/Components/ViewRoutes.js
+++ client/src/Components/ViewRoutes.js
@@ -6,6 +6,7 @@ import { Container, Col } from "react-bootstrap";
 import "../App.css";
 import API from "../API";
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 function DefaultRoute() {
   return (
@@ -30,9 +31,20 @@ function HomeRoute(props) {
 function PersonalHomeRoute(props) {
   const [studyplan, setStudyplan] = useState([]);
 
+  //NOTE think about adding a useEffect which trigger API.getStudyPlan
+
   const getStudyplan = async () => {
     const spcodes = await API.getStudyplan();
     setStudyplan(spcodes);
+  };
+
+  const deleteStudyPlan = async () => {
+    setStudyplan([]);
+    toast.promise(API.deleteStudyPlan(), {
+      pending: "Promise is pending",
+      success: "Promise resolved 👌",
+      error: "Promise rejected 🤯",
+    });
   };
 
   const saveStudyplan = async () => {
@@ -54,7 +66,12 @@ function PersonalHomeRoute(props) {
 
   return (
     <>
-      <Navbar searchBar logOutButton handleLogOut={props.handleLogOut} />
+      <Navbar
+        searchBar
+        studyplanButton
+        logOutButton
+        handleLogOut={props.handleLogOut}
+      />
       <Container>
         <h3>Courses</h3>
         <Container className="tableContainer mb-5">
@@ -65,19 +82,22 @@ function PersonalHomeRoute(props) {
             addCourseStudyplan={addCourseStudyplan}
           />
         </Container>
-        <h3>Study Plan</h3>
-        {studyplan.length > 0 ? (
-          <Container className="tableContainer">
-            <CourseTable
-              studyplan
-              loggedIn
-              courses={props.courses}
-              spcodes={studyplan}
-              deleteCourseStudyplan={deleteCourseStudyplan}
-            />
-          </Container>
+
+        {props.user && props.user.studyplan != undefined ? (
+          <>
+            <h3>Study Plan : {props.user.studyplan}</h3>
+            <Container className="tableContainer">
+              <CourseTable
+                studyplan
+                loggedIn
+                courses={props.courses}
+                spcodes={studyplan}
+                deleteCourseStudyplan={deleteCourseStudyplan}
+              />
+            </Container>
+          </>
         ) : (
-          <></>
+          <>//TODO add something here</>
         )}
       </Container>
     </>
