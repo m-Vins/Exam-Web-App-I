17e1c0b4d5b4fbb133f7383f211e06b2aa83ee31
VINCENZO MEZZELA S294999 <s294999@studenti.polito.it>
2022-06-21 12:14:47 +0200

    updated database

--- server/database.sql
+++ server/database.sql
@@ -60,7 +60,10 @@ INSERT INTO "courses" ("code","name","credits","preparatoryCourse","maxStudentsN
 INSERT INTO "courses" ("code","name","credits","preparatoryCourse","maxStudentsNumber") VALUES ('01URROV','Computational intelligence',6,NULL,NULL);
 INSERT INTO "courses" ("code","name","credits","preparatoryCourse","maxStudentsNumber") VALUES ('01OUZPD','Model based software design',4,NULL,NULL);
 INSERT INTO "courses" ("code","name","credits","preparatoryCourse","maxStudentsNumber") VALUES ('01URSPD','Internet Video Streaming',6,NULL,2);
-INSERT INTO "students" ("id","username","salt","hash","studyplan") VALUES (1,'test@uni.edu','80c6bd19d02ade025ff4618870d007ba','434458142bf7b4cff5a1b7edebd6416b0586f788e4e0221c1c0e675f8505243970a1398b40a1ca44c4e7c89cb4247ae8794ecd47d022cdab5f1554092e362d20','part-time');
+
+
+
+
 INSERT INTO "incompatibleCourses" ("code_1","code_2") VALUES ('02GOLOV','02LSEOV');
 INSERT INTO "incompatibleCourses" ("code_1","code_2") VALUES ('01SQJOV','01SQMOV');
 INSERT INTO "incompatibleCourses" ("code_1","code_2") VALUES ('01SQJOV','01SQLOV');
@@ -71,4 +74,44 @@ INSERT INTO "incompatibleCourses" ("code_1","code_2") VALUES ('05BIDOV','04GSPOV
 INSERT INTO "incompatibleCourses" ("code_1","code_2") VALUES ('01UDFOV','01TXYOV');
 INSERT INTO "incompatibleCourses" ("code_1","code_2") VALUES ('01TXYOV','01UDFOV');
 INSERT INTO "incompatibleCourses" ("code_1","code_2") VALUES ('02GRSOV','01NYHOV');
+
+
+
+INSERT INTO "students" ("id","username","salt","hash","studyplan") VALUES (1,'test1@uni.edu','80c6bd19d02ade025ff4618870d007ba','434458142bf7b4cff5a1b7edebd6416b0586f788e4e0221c1c0e675f8505243970a1398b40a1ca44c4e7c89cb4247ae8794ecd47d022cdab5f1554092e362d20','part-time');
+INSERT INTO "students" ("id","username","salt","hash","studyplan") VALUES (2,'test2@uni.edu','80c6bd19d02ade025ff4618870d007ba','434458142bf7b4cff5a1b7edebd6416b0586f788e4e0221c1c0e675f8505243970a1398b40a1ca44c4e7c89cb4247ae8794ecd47d022cdab5f1554092e362d20','full-time');
+INSERT INTO "students" ("id","username","salt","hash","studyplan") VALUES (3,'test3@uni.edu','80c6bd19d02ade025ff4618870d007ba','434458142bf7b4cff5a1b7edebd6416b0586f788e4e0221c1c0e675f8505243970a1398b40a1ca44c4e7c89cb4247ae8794ecd47d022cdab5f1554092e362d20','full-time');
+INSERT INTO "students" ("id","username","salt","hash","studyplan") VALUES (4,'test4@uni.edu','80c6bd19d02ade025ff4618870d007ba','434458142bf7b4cff5a1b7edebd6416b0586f788e4e0221c1c0e675f8505243970a1398b40a1ca44c4e7c89cb4247ae8794ecd47d022cdab5f1554092e362d20','part-time');
+INSERT INTO "students" ("id","username","salt","hash","studyplan") VALUES (5,'test5@uni.edu','80c6bd19d02ade025ff4618870d007ba','434458142bf7b4cff5a1b7edebd6416b0586f788e4e0221c1c0e675f8505243970a1398b40a1ca44c4e7c89cb4247ae8794ecd47d022cdab5f1554092e362d20',NULL);
+
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (1,'01UDFOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (1,'03UEWOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (1,'01URROV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (1,'01OTWOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (1,'01NYHOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (2,'01UDFOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (2,'02GOLOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (2,'03UEWOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (2,'01URROV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (2,'01OTWOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (2,'01SQJOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (2,'01TYMOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (2,'05BIDOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (2,'01URSPD');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (2,'01OUZPD');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (2,'01NYHOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (3,'01OTWOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (3,'01NYHOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (3,'01TXYOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (3,'01TXSOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (3,'01TYDOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (3,'01UDUOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (3,'01SQOOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (3,'01SQMOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (3,'01TYMOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (3,'01URSPD');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (4,'01TYDOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (4,'01OUZPD');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (4,'01TYMOV');
+INSERT INTO "studyPlans" ("studentID","courseID") VALUES (4,'01SQLOV');
+
 COMMIT;
